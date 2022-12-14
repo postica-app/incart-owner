aa7cd9040aab492c61cc287dd58ab1c103362f1c
rycont <rycont@outlook.kr>
2022-12-14 17:04:55 +0900

    fix: remove top level await for legacy browsers

--- src/main.tsx
+++ src/main.tsx
@@ -16,35 +16,43 @@ declare global {
 
 window.supabase = supabase
 
-const pages = await Promise.all(
-    Object.entries(import.meta.glob('/src/pages/**/page.tsx')).map(
-        async ([path, importTarget]) => {
-            const target = (await importTarget()) as {
-                default: React.FC
-                info: {
-                    group: string
-                    name: string
+const main = async () => {
+    const pages = await Promise.all(
+        Object.entries(import.meta.glob('/src/pages/**/page.tsx')).map(
+            async ([path, importTarget]) => {
+                const target = (await importTarget()) as {
+                    default: React.FC
+                    info: {
+                        group: string
+                        name: string
+                    }
+                    dataLoader: () => unknown
                 }
-                dataLoader: () => unknown
-            }
 
-            return {
-                path: path.split('/').slice(3, -1).join('/').replace('$', ':'),
-                element: <target.default />,
-                loader: target.dataLoader,
+                return {
+                    path: path
+                        .split('/')
+                        .slice(3, -1)
+                        .join('/')
+                        .replace('$', ':'),
+                    element: <target.default />,
+                    loader: target.dataLoader,
+                }
             }
-        }
+        )
     )
-)
 
-const router = createBrowserRouter([
-    { path: '/', element: <Layout />, children: pages },
-])
+    const router = createBrowserRouter([
+        { path: '/', element: <Layout />, children: pages },
+    ])
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <Modal />
-        <Toaster />
-        <RouterProvider router={router} />
-    </React.StrictMode>
-)
+    ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+        <React.StrictMode>
+            <Modal />
+            <Toaster />
+            <RouterProvider router={router} />
+        </React.StrictMode>
+    )
+}
+
+main()
