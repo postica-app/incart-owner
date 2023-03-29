da3ed03a75659164a201f254c018559bf38c9db2
rycont <rycont>
2023-03-29 04:09:53 +0000

    fix: main page redirection issue

--- src/pages/page.tsx
+++ src/pages/page.tsx
@@ -3,8 +3,10 @@ import { useNavigate } from 'react-router-dom'
 
 export default () => {
     const goto = useNavigate()
+
     useEffect(() => {
         goto('/product')
-    }, [])
-    return <>아니진짜요?</>
+    }, [goto])
+
+    return <></>
 }
