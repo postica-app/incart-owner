9d474947cc1699242c8fd7c59928af4d7102ffe9
rycont <rycont>
2023-04-30 15:20:19 +0000

    fix: update db fetcher to query partial string

--- src/pages/order/actions.tsx
+++ src/pages/order/actions.tsx
@@ -2,11 +2,11 @@ import { OrderStage, ProductType } from 'incart-fe-common'
 import { supabase } from '@/supabase'
 import { z } from 'zod'
 
-const OrderFilter = z.object({
+export const OrderFilter = z.object({
     range_start: z.string().regex(/^\d{4}-\d{2}-\d{2}$/),
     range_end: z.string().regex(/^\d{4}-\d{2}-\d{2}$/),
     product_id: z.string().uuid().optional(),
-    order_status: z
+    order_stage: z
         .enum([
             OrderStage.created,
             OrderStage.paid,
@@ -14,39 +14,41 @@ const OrderFilter = z.object({
             OrderStage.canceled,
         ])
         .optional(),
-    order_id: z.string().uuid().optional(),
+    rid: z.string().uuid().optional(),
     orderer_name: z.string().optional(),
     orderer_phone: z.string().optional(),
-    orderer_email: z.string().email().optional(),
+    orderer_email: z.string().optional(),
+    receiver_name: z.string().optional(),
+    receiver_phone: z.string().optional(),
+    shipping_info: z.string().optional(),
 })
 
 export default {
-    async getOrdersWithFilter(_filter: z.infer<typeof OrderFilter> | unknown) {
-        const filter = OrderFilter.parse(_filter)
-
+    async getOrdersWithFilter(filter: z.infer<typeof OrderFilter>) {
+        console.log(filter)
         let query = supabase
             .from('order_sheet')
-            .select('rid,id,orderer_name,shipping_info,status')
+            .select('rid,id,orderer_name,shipping_info,stage')
             .gte('created_at', filter.range_start)
             .lte('created_at', filter.range_end)
 
-        if (filter.order_status) query = query.eq('status', filter.order_status)
+        if (filter.order_stage) query = query.eq('stage', filter.order_stage)
 
-        if (filter.order_id) query = query.eq('id', filter.order_id)
+        if (filter.rid) query = query.textSearch('rid', filter.rid)
 
         if (filter.orderer_name)
-            query = query.eq('orderer_name', filter.orderer_name)
+            query = query.like('orderer_name', `%${filter.orderer_name}%`)
 
         if (filter.orderer_phone)
-            query = query.eq('orderer_phone', filter.orderer_phone)
+            query = query.like('orderer_phone', `%${filter.orderer_phone}%`)
 
         if (filter.orderer_email)
-            query = query.eq('orderer_email', filter.orderer_email)
+            query = query.like('orderer_email', `%${filter.orderer_email}%`)
 
         const { data: orders, error: orderError } = await query
 
         if (orders === null || orderError) {
-            console.error(orderError)
+            console.error('오류 발생!', orderError)
             throw new Error(
                 orderError?.message || 'Error while fetching orders'
             )
@@ -69,7 +71,6 @@ export default {
             await orderItemsQuery
 
         if (orderItems === null || orderItemsError) {
-            console.error(orderError)
             throw new Error(
                 orderItemsError?.message ||
                     'Error while fetching order products'
